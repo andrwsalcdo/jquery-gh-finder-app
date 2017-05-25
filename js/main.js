6d6aec952d00b740a4d1effd857c9d301523634c
Andrew Salcedo <asalcedo07@gmail.com>
2017-05-24 22:32:35 -0400

    add basic GH user profile info. todo: latest repos

--- js/main.js
+++ js/main.js
@@ -12,7 +12,36 @@ $(document).ready(function() {
       // } // you would use this if you want more than 50 requests per hour
             // but it isn't necessary for this simple project
     }).done(function(user) {
-      console.log(user); //gets api data associated with the GH username
+      // console.log(user); //gets api data associated with the GH username
+
+        $('#profile').html(`
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              <h3 class="panel-title">${user.name}</h3>
+            </div>
+            <div class="panel-body">
+              <div class="row">
+                <div class="col-md-3">
+                  <img class="thumbnail avatar" src="${user.avatar_url}">
+                  <a target="_blank" class="btn btn-primary btn-block" href="${user.html_url}">View Profile</a>
+                </div>
+                <div class="col-md-9">
+                  <span class="label label-default">Public Repos: ${user.public_repos}</span>
+                  <span class="label label-primary">Public Gists: ${user.public_gists}</span>
+                  <span class="label label-success">Followers: ${user.followers}</span>
+                  <span class="label label-info">Following: ${user.following}</span>
+                  <br><br>
+                  <ul class="list-group">
+                    <li class="list-group-item">Company: ${user.company}</li>
+                    <li class="list-group-item">Website: ${user.blog}</li>
+                    <li class="list-group-item">Location: ${user.location}</li>
+                    <li class="list-group-item">Member Since: ${user.created_at}</li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        `);
     });
   });
 });
